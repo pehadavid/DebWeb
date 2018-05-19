baace1711408244fa5453ea3571b5d4b0b2a4153
Pierre-Alain David <pa@nextinpact.com>
2018-05-19 09:31:24 +0200

    bootstrap project

---
+++ DebWeb/DebWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DebWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
